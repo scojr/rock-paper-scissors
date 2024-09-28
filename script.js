66073f4ded945f736461af9e8b7a1e03d162850e
scojr <180873402+scojr@users.noreply.github.com>
2024-09-28 14:21:23 -0500

    fix: add condition for invalid user choice

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!")
-
 // function getComputerChoice randomly chooses between the strings "rock", "paper" or "scissors".
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3));
@@ -25,33 +23,40 @@ let computerScore = 0;
 function playRound(computerChoice,humanChoice) {
     if (computerChoice === "rock" && humanChoice === "paper") {
         humanScore++;
-        announceWinner("Human")
+        announceWinner("human")
     } else if (computerChoice === "rock" && humanChoice === "scissors") {
         computerScore++;
-        announceWinner("Computer")
+        announceWinner("computer")
     } else if (computerChoice === "paper" && humanChoice === "rock") {
         computerScore++;
-        announceWinner("Computer")
+        announceWinner("computer")
     } else if (computerChoice === "paper" && humanChoice === "scissors") {
         humanScore++;
-        announceWinner("Human")
+        announceWinner("human")
     } else if (computerChoice === "scissors" && humanChoice === "rock") {
         humanScore++;
-        announceWinner("Human")
+        announceWinner("human")
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
         computerScore++;
-        announceWinner("Computer")
+        announceWinner("computer")
+    } else if (computerChoice === humanChoice){
+        announceWinner("tie");
     } else {
-        announceWinner(false);
+        computerScore++;
+        announceWinner("invalid");
     }
 }
 
 // function announceWinner dynamically logs a message into the console declaring the winner every round.
 function announceWinner(winner) {
-    if (winner) {
-        console.log(`Human chose ${humanSelection}, and computer chose ${computerSelection}. ${winner} wins! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
-    } else {
+    if (winner === "computer") {
+        console.log(`Human chose ${humanSelection}, and computer chose ${computerSelection}. Computer wins! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
+    } else if (winner === "human") {
+        console.log(`Human chose ${humanSelection}, and computer chose ${computerSelection}. Human wins! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
+    } else if (winner === "tie") {
         console.log(`Human chose ${humanSelection}, and computer chose ${computerSelection}. It's a tie! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
+    } else {
+        console.log(`Human chose "${humanSelection}", an invalid answer. Computer wins by default! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
     }
 }
 
