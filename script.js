8434e9e2a9407c9f3f9d8df6f2df3e9d0f0aeae8
scojr <180873402+scojr@users.noreply.github.com>
2024-09-28 10:08:33 -0500

    feat: add function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+// function getComputerChoice randomly chooses between the strings "rock", "paper" or "scissors".
+
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 3) + 1);
+    if (choice === 1) {
+        return "rock";
+    } else if (choice == 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// function getHumanChoice uses a prompt method to allow the user to input their choice between "rock", "paper" or "scissors".
+
