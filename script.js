8003a97f6429eff246368adc1eea946ba6f1bc99
scojr <180873402+scojr@users.noreply.github.com>
2024-09-28 10:53:47 -0500

    feat: add playRound(), announceWinner() functions

--- script.js
+++ script.js
@@ -17,3 +17,45 @@ function getHumanChoice() {
     let choice = prompt("Choose \"Rock\", \"Paper\" or \"Scissors\"");
     return choice.toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+// function playRound takes the return value of getComputerChoice() and getPlayerChoice() as arguments, plays a single round, increments the appropriate round winner's score, and logs a winner announcement.
+function playRound(computerChoice,humanChoice) {
+    if (computerChoice === "rock" && humanChoice === "paper") {
+        humanScore++;
+        announceWinner("Human")
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        computerScore++;
+        announceWinner("Computer")
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        computerScore++;
+        announceWinner("Computer")
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        humanScore++;
+        announceWinner("Human")
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore++;
+        announceWinner("Human")
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore++;
+        announceWinner("Computer")
+    } else {
+        announceWinner(false);
+    }
+}
+
+// function announceWinner dynamically logs a message into the console declaring the winner every round.
+function announceWinner(winner) {
+    if (winner) {
+        console.log(`Human chose ${humanSelection}, and computer chose ${computerSelection}. ${winner} wins! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
+    } else {
+        console.log(`Human chose ${humanSelection}, and computer chose ${computerSelection}. It's a tie! The scores are Human: ${humanScore}, Computer: ${computerScore}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(computerSelection, humanSelection);
